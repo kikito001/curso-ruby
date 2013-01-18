c408b48525e3fab67129945e5e485d453e06f3a6
kikito.001 <kikito.001@gmail.com>
2013-01-18 15:10:55 -0500

    003-Este es un commit del archivo donde se puede ver el uso de los comentarios

---
+++ 003-Comentarios.rb
@@ -0,0 +1,17 @@
+#Comentarios , para documentar los programa qu se hagan
+# para comentar de una sola linea se usa #
+=begin
+Esto es un comentario 
+de varias lineas para poder
+comentar temas mas amplios	
+=end
+class HolaMundo
+	def initialize()
+		
+	end
+	def saluda()
+		puts "Para los comentarios de una linea se usa # "
+	end
+end
+	objeto = HolaMundo.new()
+	objeto.saluda()
